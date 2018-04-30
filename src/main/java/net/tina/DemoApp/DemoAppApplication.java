eaf0b8a150ad2999aac70af3c0ad6623126f9df8
Tina <tina.temishova@gmail.com>
2018-04-30 23:54:38 +0600

    Initial project

---
+++ src/main/java/net/tina/DemoApp/DemoAppApplication.java
@@ -0,0 +1,26 @@
+package net.tina.DemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+	
+	@Bean
+	public ViewResolver getViewResolver()
+	{
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/jsp/");
+	    resolver.setSuffix(".jsp");
+	    resolver.setViewClass(JstlView.class);
+	    return resolver;
+	}
+}
